f7c9f08dda7821d69d0c7e2fdd55c86491aa087a
LukkzHS <luccahs03@gmail.com>
2024-08-28 15:26:39 -0300

    fix imagem fundo form

--- src/components/main/ContactModal/ContactModal.stories.tsx
+++ src/components/main/ContactModal/ContactModal.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type {Meta, StoryObj} from '@storybook/react';
 
-import {ContactModal} from './ContactModal';
+import ContactModal from './ContactModal';
 
 const meta: Meta<typeof ContactModal> = {
   component: ContactModal,
