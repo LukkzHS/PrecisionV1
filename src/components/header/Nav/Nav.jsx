f9b06f4a284d9647e0deb853ab85f2a4a878cc4f
LukkzHS <luccahs03@gmail.com>
2024-09-11 16:08:42 -0300

    fix header menu

--- src/components/header/Nav/Nav.jsx
+++ src/components/header/Nav/Nav.jsx
@@ -29,7 +29,6 @@ function Nav() {
                     <li className={styles.hideOnMobile}><a href="#contatos">Contatos</a></li>
                     <li className={styles.menuButton} onClick={(event) => {showSidebar(); handleLinkClick(event);}}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
                 </ul>
-;
             </div>
             
         </nav>
